3313088b4175743795a778c1385515104dcd9a6c
khuzama98 <skhuzamashahid@gmail.com>
2019-05-29 11:20:19 +0500

    date time picker added

--- src/components/forms/DatePicker.js
+++ src/components/forms/DatePicker.js
@@ -1,6 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import $ from "jquery";
+import DatePicker from "react-datepicker";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
 
-export default class DatePicker extends Component {
+import "react-datepicker/dist/react-datepicker.css";
+export default class datePicker extends Component {
+  // toggle = () =>{
+  //   $(function() {
+  //   $('#datetimepicker1').datetimepicker();
+  // });}
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date()
+    };
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
   render() {
     return (
       <>
@@ -10,6 +34,7 @@ export default class DatePicker extends Component {
             <p className="lead text-muted">Showcasing multiple date pickers</p>
           </div>
         </header>
+
         <div className="content">
           <div className="card-columns columns-md-2">
             <div className="card border-0 shadow-box">
@@ -19,95 +44,86 @@ export default class DatePicker extends Component {
               <div className="card-body">
                 <form action=";" className="cozy">
                   <div className="form-group row">
-                    <label className="col-form-label col-md-3">Minimum Setup</label>
+                    <label className="col-form-label col-md-3">
+                      Default Setup
+                    </label>
                     <div className="col-md-9">
-                      <input type="text" className="form-control" id="datepicker-default" placeholder="Select a date" readonly="readonly" />
+                      <DatePicker
+                        selected={this.state.startDate}
+                        onChange={this.handleChange}
+                      />
+                      {/* <input type="text" className="form-control" id="datetimepicker1" placeholder="Select a date"  /> */}
                     </div>
                   </div>
                   <div className="form-group row">
-                    <label className="col-form-label col-md-3">Input Group Setup</label>
-                    <div className="col-md-9">
-                      <div className="input-group">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </div>
-                        <input type="text" className="form-control" id="datepicker-inputgroup" placeholder="Select a date" readonly="readonly" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-md-3">Enable Helper Buttons</label>
+                    <label className="col-form-label col-md-3">
+                      Select Time Setup
+                    </label>
                     <div className="col-md-9">
                       <div className="input-group">
                         <div className="input-group-prepend">
                           <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
+                            <i className="far fa-calendar-alt" />
                           </span>
                         </div>
-                        <input type="text" className="form-control" value="11/30/2018" id="datepicker-helpers" readonly="readonly" />
+                        <DatePicker
+                          selected={this.state.startDate}
+                          onChange={this.handleChange}
+                          showTimeSelect
+                          timeFormat="HH:mm"
+                          timeIntervals={15}
+                          dateFormat="MMMM d, yyyy h:mm aa"
+                          timeCaption="time"
+                        />
                       </div>
                     </div>
                   </div>
                   <div className="form-group row">
-                    <label className="col-form-label col-md-3">Position</label>
+                    <label className="col-form-label col-md-3">
+                      Select Time Only Setup
+                    </label>
                     <div className="col-md-9">
-                      <div className="input-group mb-3">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </div>
-                        <input type="text" className="form-control" placeholder="Top left" id="datepicker-tl" />
-                      </div>
-                      <div className="input-group mb-3">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </div>
-                        <input type="text" className="form-control" placeholder="Top right" id="datepicker-tr" />
-                      </div>
-                      <div className="input-group mb-3">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </div>
-                        <input type="text" className="form-control" placeholder="Bottom left" id="datepicker-bl" />
-                      </div>
                       <div className="input-group">
                         <div className="input-group-prepend">
                           <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
+                            <i className="far fa-calendar-alt" />
                           </span>
                         </div>
-                        <input type="text" className="form-control" placeholder="Bottom right" id="datepicker-br" />
+                        <DatePicker
+                          selected={this.state.startDate}
+                          onChange={this.handleChange}
+                          showTimeSelect
+                          showTimeSelectOnly
+                          timeIntervals={15}
+                          dateFormat="h:mm aa"
+                          timeCaption="Time"
+                        />
                       </div>
                     </div>
                   </div>
                   <div className="form-group row">
-                    <label className="col-form-label col-md-3">Range Picker</label>
+                    <label className="col-form-label col-md-3">Exclude Times Setup</label>
                     <div className="col-md-9">
-                      <div className="input-group input-daterange" id="datepicker-range">
-                        <input type="text" className="form-control" name="startDate" />
-                        <div className="input-group-append input-group-prepend">
+                      <div className="input-group mb-3">
+                        <div className="input-group-prepend">
                           <span className="input-group-text">
-                            <i className="fas fa-angle-right"></i>
+                            <i className="far fa-calendar-alt" />
                           </span>
                         </div>
-                        <input type="text" className="form-control" name="endDate" />
+                        <DatePicker
+                          selected={this.state.startDate}
+                          onChange={this.handleChange}
+                          showTimeSelect
+                          excludeTimes={[setHours(setMinutes(new Date(), 0), 17), setHours(setMinutes(new Date(), 30), 18), setHours(setMinutes(new Date(), 30), 19), setHours(setMinutes(new Date(), 30), 17)]}
+                          dateFormat="MMMM d, yyyy h:mm aa"
+                        />
                       </div>
                     </div>
                   </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-lg-3 col-sm-12">Inline Mode</label>
-                    <div className="col-lg-4 col-md-9 col-sm-12">
-                      <div className="" id="datepicker-inline"></div>
-                      <input type="hidden" id="datepicker-inline-value" />
-                    </div>
-                  </div>
+                  
+                    {/* </div> */}
+                  {/* </div> */}
+                 
                 </form>
               </div>
             </div>
@@ -118,142 +134,79 @@ export default class DatePicker extends Component {
               <div className="card-body">
                 <form action=";" className="cozy">
                   <div className="form-group row">
-                    <label className="col-form-label col-md-3">Basic options</label>
+                    <label className="col-form-label col-md-3">
+                      Include Times Setup
+                    </label>
                     <div className="col-md-9">
                       <div className="input-group">
                         <span className="input-group-prepend">
                           <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
+                            <i className="far fa-calendar-alt" />
                           </span>
                         </span>
-                        <input type="text" className="form-control pickadate" placeholder="Pick a date..." />
+                        <DatePicker
+                          selected={this.state.startDate}
+                          onChange={this.handleChange}
+                          showTimeSelect
+                          includeTimes={[setHours(setMinutes(new Date(), 0), 17), setHours(setMinutes(new Date(), 30), 18), setHours(setMinutes(new Date(), 30), 19), setHours(setMinutes(new Date(), 30), 17)]}
+                          dateFormat="MMMM d, yyyy h:mm aa"
+                        />
                       </div>
                     </div>
                   </div>
                   <div className="form-group row">
-                    <label className="col-form-label col-md-3">Header labels</label>
+                    <label className="col-form-label col-md-3">
+                      Inject Specific Times Setup
+                    </label>
                     <div className="col-md-9">
                       <div className="input-group">
                         <span className="input-group-prepend">
                           <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
+                            <i className="far fa-calendar-alt" />
                           </span>
                         </span>
-                        <input type="text" className="form-control pickadate-header-labels" placeholder="Pick a date..." />
+                        <DatePicker
+                          selected={this.state.startDate}
+                          onChange={this.handleChange}
+                          showTimeSelect
+                          timeFormat="HH:mm"
+                          injectTimes={[
+                            setHours(setMinutes(new Date(), 1), 0),
+                            setHours(setMinutes(new Date(), 5), 12),
+                            setHours(setMinutes(new Date(), 59), 23)
+                          ]}
+                          dateFormat="MMMM d, yyyy h:mm aa"
+                        />
                       </div>
                     </div>
                   </div>
                   <div className="form-group row">
-                    <label className="col-form-label col-md-3">Buttons</label>
+                    <label className="col-form-label col-md-3">Specific Time Range Setup</label>
                     <div className="col-md-9">
                       <div className="input-group">
                         <span className="input-group-prepend">
                           <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
+                            <i className="far fa-calendar-alt" />
                           </span>
                         </span>
-                        <input type="text" className="form-control pickadate-buttons" placeholder="Pick a date..." />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-md-3">Formats</label>
-                    <div className="col-md-9">
-                      <div className="input-group">
-                        <span className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </span>
-                        <input type="text" className="form-control pickadate-format" placeholder="Pick a date..." />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-md-3">Year selector</label>
-                    <div className="col-md-9">
-                      <div className="input-group">
-                        <span className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </span>
-                        <input type="text" className="form-control pickadate-year" placeholder="Pick a date..." />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-md-3">Editable input</label>
-                    <div className="col-md-9">
-                      <div className="input-group">
-                        <span className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </span>
-                        <input type="text" className="form-control pickadate-editable" placeholder="Pick a date..." />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-md-3">Dropdown selectors</label>
-                    <div className="col-md-9">
-                      <div className="input-group">
-                        <span className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </span>
-                        <input type="text" className="form-control pickadate-selectors" placeholder="Pick a date..." />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-md-3">Disable ranges</label>
-                    <div className="col-md-9">
-                      <div className="input-group">
-                        <span className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </span>
-                        <input type="text" className="form-control pickadate-disable-range" placeholder="Pick a date..." />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-md-3">Events</label>
-                    <div className="col-md-9">
-                      <div className="input-group">
-                        <span className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </span>
-                        <input type="text" className="form-control pickadate-events" placeholder="Open your console and Pick a date..." />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-form-label col-md-3">Disable dates</label>
-                    <div className="col-md-9">
-                      <div className="input-group">
-                        <span className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="far fa-calendar-alt"></i>
-                          </span>
-                        </span>
-                        <input type="text" className="form-control pickadate-disable" placeholder="Pick a date..." />
+                        <DatePicker
+                          selected={this.state.startDate}
+                          onChange={this.handleChange}
+                          showTimeSelect
+                          minTime={setHours(setMinutes(new Date(), 0), 17)}
+                          maxTime={setHours(setMinutes(new Date(), 30), 20)}
+                          dateFormat="MMMM d, yyyy"
+                        />
                       </div>
                     </div>
                   </div>
                 </form>
               </div>
             </div>
-            <div className="card border-0 shadow-box"></div>
+            <div className="card border-0 shadow-box" />
           </div>
         </div>
       </>
-    )
+    );
   }
 }
